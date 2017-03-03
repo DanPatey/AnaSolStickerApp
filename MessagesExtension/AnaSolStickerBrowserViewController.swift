5054385f6eea1a68a821607c3675f3ccfe23cd5c
Dan Patey <Dan@StackSocial.com>
2017-03-03 10:01:57 -0800

    First batch of images

---
+++ MessagesExtension/AnaSolStickerBrowserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AnaSolStickerBrowserViewController.swift
+//  AnaSolStickerApp
+//
+//  Created by Dan Patey on 3/3/17.
+//  Copyright © 2017 Dan Patey. All rights reserved.
+//
+
+import Messages
+
+let stickerNames = ["ChocolateMelon", "ChocolateBanana", "ChocolatePineapple", "ChocolateStrawberry", "Ginger", "Kiwi", "Lemon", "Melon", "Banana", "Orange", "Pineapple", "Strawberry"]
+
+class AnaSolStickerBrowserViewController: MSStickerBrowserViewController {
+    var stickers = [MSSticker]()
+    
+}
