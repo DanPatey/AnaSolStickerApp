b0241fa4ac74036609b169a98758b0560c210263
Dan Patey <Dan@StackSocial.com>
2017-03-03 10:46:28 -0800

    Starting on chocomanic mode

--- MessagesExtension/AnaSolStickerBrowserViewController.swift
+++ MessagesExtension/AnaSolStickerBrowserViewController.swift
@@ -11,6 +11,37 @@ import Messages
 let stickerNames = ["ChocolateMelon", "ChocolateBanana", "ChocolatePineapple", "ChocolateStrawberry", "Ginger", "Kiwi", "Lemon", "Melon", "Banana", "Orange", "Pineapple", "Strawberry"]
 
 class AnaSolStickerBrowserViewController: MSStickerBrowserViewController {
+    
     var stickers = [MSSticker]()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadStickers()
+        stickerBrowserView.backgroundColor = #colorLiteral(
+            red:  0.9490196078, green: 0.7568627451,
+            blue: 0.8196078431, alpha: 1)
+    }
+    
+}
+
+extension AnaSolStickerBrowserViewController {
+    
+    func loadStickers() {
+        stickers = stickerNames.map({ name in
+            let url = Bundle.main.url(forResource: name, withExtension: "png")!
+            return try! MSSticker(contentsOfFileURL: url, localizedDescription: name)
+        })
+    }
+    
+}
+
+//MARK: MSStickerBrowserViewDataSource
+extension AnaSolStickerBrowserViewController {
+    override func numberOfStickers(in stickerBrowserView: MSStickerBrowserView) -> Int {
+        return stickers.count
+    }
+    
+    override func stickerBrowserView(_ stickerBrowserView: MSStickerBrowserView, stickerAt index: Int) -> MSSticker {
+        return stickers[index]
+    }
 }
--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -10,5 +10,5 @@ import UIKit
 import Messages
 
 class MessagesViewController: MSMessagesAppViewController {
-    
+
 }
